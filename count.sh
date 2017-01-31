241907a0e4d9f2f1eea2a9d1721c1c36d3ebb295
zaoqi <zaoqiff@gmail.com>
2017-01-31 22:57:06 +0800

    增加count.sh

---
+++ count.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+n() {
+	if echo "$*"|grep '^[[:digit:]]*$'>/dev/null ;then
+		echo "$*"
+	else
+		echo 0
+	fi
+}
+papers=0
+paperDones=0
+voids=0
+voidDones=0
+for paper in $(cat "$@" | awk '{print $2}') ;do
+	let papers+=$(n $paper)
+done
+for void in $(cat "$@" | awk '{print $3}') ;do
+	let voids+=$(n $void)
+done
+for paperDone in $(cat "$@" | awk '{print $5}') ;do
+	let paperDones+=$(n $paperDone)
+done
+for voidDone in $(cat "$@" | awk '{print $6}') ;do
+	let voidDones+=$(n $voidDone)
+done
+echo "试卷：$paperDones/$papers，白纸：$voidDones/$voids"
